e97dc987c32f16b51f82cd485a2a36fa2004fbd5
Alex Cera <alexcera007@gmail.com>
2014-07-02 18:16:19 +0800

    results are now generated via js. not hardcoded on html.

--- js/data.js
+++ js/data.js
@@ -2,101 +2,118 @@ var DATA = [
     {
         name: "Bay Hotel",
         loc: "Telok Blangah Rd",
-        dist: "",
+        dist: 10,
         img: "img/bay-hotel.jpg",
         price: "150",
         deal_price: "150",
         currency: "$",
-        star_rating: "3"
+        star_rating: 3
     },
     {
         name: "Crowne Plaza Hotel",
         loc: "Changi Airport",
-        dist: "",
+        dist: 20,
         img: "img/crown-plaza.jpg",
         price: "275",
         deal_price: "270",
         currency: "S$",
-        star_rating: "4"
+        star_rating: 4
     },
     {
         name: "Dorsett Regency",
         loc: "New Bridge Rd",
-        dist: "",
+        dist: 3,
         img: "img/dorset-regency.jpg",
         price: "260",
         deal_price: "260",
         currency: "S$",
-        star_rating: "4"
+        star_rating: 4
     },
     {
         name: "The Fullerton Hotel Singapore",
         loc: "Fullerton Square",
-        dist: "",
+        dist: 7,
         img: "img/fullerton.jpg",
         price: "300",
         deal_price: "300",
         currency: "S$",
-        star_rating: "5"
+        star_rating: 5
     },
     {
         name: "Link Hotel",
         loc: "Tiong Bahru Rd",
-        dist: "",
+        dist: 13,
         img: "img/link-hotel.jpg",
         price: "150",
         deal_price: "150",
         currency: "S$",
-        star_rating: "3"
+        star_rating: 3
     },
     {
         name: "Marina Bay Sands",
         loc: "Bayfront Ave",
-        dist: "",
+        dist: 11,
         img: "img/marina-bay-sands.jpg",
         price: "400",
         deal_price: "380",
         currency: "S$",
-        star_rating: "5"
+        star_rating: 5
     },
     {
         name: "Singapore Marriott Hotel",
         loc: "Orchard Rd",
-        dist: "",
+        dist: 11,
         img: "img/marriott.jpg",
         price: "360",
         deal_price: "360",
         currency: "S$",
-        star_rating: "5"
+        star_rating: 5
     },
     {
         name: "Raffles Hotel",
         loc: "Beach Rd",
-        dist: "",
+        dist: 8,
         img: "img/raffles-hotel.jpg",
         price: "290",
         deal_price: "290",
         currency: "S$",
-        star_rating: "4"
+        star_rating: 4
     },
     {
         name: "Rendezvous Hotel",
         loc: "Bras Basah Rd",
-        dist: "",
+        dist: 9,
         img: "img/rendezvous-hotel.jpg",
         price: "350",
         deal_price: "350",
         currency: "S$",
-        star_rating: "4"
+        star_rating: 4
     },
     {
         name: "Trader's Hotel",
         loc: "Cuscaden Rd",
-        dist: "",
+        dist: 12,
         img: "img/traders-hotel.jpg",
         price: "220",
         deal_price: "200",
         currency: "S$",
-        star_rating: "3"
+        star_rating: 3
     }
 ];
+
+
+DATA.sortAscByProp = function(prop) {
+    var temp = this.slice(0);
+    temp.sort(function(a, b) {
+        return a[prop] - b[prop];
+    });
+    return temp;
+};
+
+DATA.sortDescByProp = function(prop) {
+    var temp = this.slice(0);
+    temp.sort(function(a, b) {
+        return b[prop] - a[prop];
+    });
+    return temp;
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,101 @@
-
-
-
 window.onload = function() {
-    
 
-    
+    //get the default selected option
+    //load the results using the default option
+
+    //attach change handler on the location drop-down
+    var container = document.querySelector(".result-container");
+    dataRenderer.showData(DATA, "star_rating", container);
+
 };
+
+
+var dataRenderer = (function(w) {
+
+    function constructDOMFragment() {
+
+        var fragment = document.createDocumentFragment();
+        for (var i = results.length - 1; i >= 0; i--)(function(r) {
+
+            var div_container = document.createElement("div");
+            div_container.className = "pull-left col-xs-4";
+
+            var div_thumbnail = document.createElement("div");
+            div_thumbnail.className = "thumbnail";
+
+            div_container.appendChild(div_thumbnail);
+
+            var div_caption_top = document.createElement("div");
+            div_caption_top.className = "caption";
+
+            var h5 = document.createElement("h5");
+            h5["innerHTML" || "textContent"] = r.name;
+            div_caption_top.appendChild(h5);
+            div_thumbnail.appendChild(div_caption_top);
+
+            var img = document.createElement("img");
+            img.className = "img-responsive";
+            img.src = r.img;
+            img.alt = r.name;
+
+            div_thumbnail.appendChild(img);
+
+            var div_caption_bottom = document.createElement("div");
+            div_caption_bottom.className = "caption";
+
+            var h3_price = document.createElement("h3");
+            h3_price.className = "pull-right";
+            h3_price["innerHTML" || "textContent"] = r.price;
+
+            div_caption_bottom.appendChild(h3_price);
+
+            var clearfix = document.createElement("div");
+            clearfix.className = "clearfix";
+            div_caption_bottom.appendChild(clearfix);
+
+            div_thumbnail.appendChild(div_caption_bottom);
+            fragment.appendChild(div_container);
+
+        })(results[i]);
+
+        // "<div class=\"pull-left col-xs-4\">"
+        //     "<div class=\"thumbnail\">"
+        //         "<div class=\"caption\">"
+        //             "<h5>Miami</h5>"
+        //         "</div>"
+        //         "<img class=\"img-responsive\" src=\"\" alt=\"\">"
+        //         "<div class=\"caption\">"               
+        //             "<h3 class=\"pull-right\">$177</h3>"
+        //             "<div class=\"clearfix\"></div>"
+        //         "</div>"
+        //     "</div>"
+        // "</div>"
+
+        var clearfix = document.createElement("div");
+        clearfix.className = "clearfix";
+        fragment.appendChild(clearfix);
+
+        return fragment;
+    };
+
+    function showData(d, filter, container) {
+        results = d.slice(0);
+        if (filter === "star_rating") {
+            results.sort(function(a, b) {
+                return b.star_rating - a.star_rating;
+            });
+        } else if (filter === "dist") {
+            results.sort(function(a, b) {
+                return a.dist - b.dist;
+            });
+        }
+
+        var f = constructDOMFragment();
+        container.appendChild(f);
+    };
+
+    return {
+        showData: showData
+    };
+
+})(window);
