06ce9fe79f02030719e87f0bd6fd10bac35b3aac
Alex Cera <alexcera007@gmail.com>
2014-07-02 15:38:27 +0800

    add image folder and some images of varying sizes. some layout changes.

---
+++ js/data.js
@@ -0,0 +1,102 @@
+var DATA = [
+    {
+        name: "Bay Hotel",
+        loc: "Telok Blangah Rd",
+        dist: "",
+        img: "img/bay-hotel.jpg",
+        price: "150",
+        deal_price: "150",
+        currency: "$",
+        star_rating: "3"
+    },
+    {
+        name: "Crowne Plaza Hotel",
+        loc: "Changi Airport",
+        dist: "",
+        img: "img/crown-plaza.jpg",
+        price: "275",
+        deal_price: "270",
+        currency: "S$",
+        star_rating: "4"
+    },
+    {
+        name: "Dorsett Regency",
+        loc: "New Bridge Rd",
+        dist: "",
+        img: "img/dorset-regency.jpg",
+        price: "260",
+        deal_price: "260",
+        currency: "S$",
+        star_rating: "4"
+    },
+    {
+        name: "The Fullerton Hotel Singapore",
+        loc: "Fullerton Square",
+        dist: "",
+        img: "img/fullerton.jpg",
+        price: "300",
+        deal_price: "300",
+        currency: "S$",
+        star_rating: "5"
+    },
+    {
+        name: "Link Hotel",
+        loc: "Tiong Bahru Rd",
+        dist: "",
+        img: "img/link-hotel.jpg",
+        price: "150",
+        deal_price: "150",
+        currency: "S$",
+        star_rating: "3"
+    },
+    {
+        name: "Marina Bay Sands",
+        loc: "Bayfront Ave",
+        dist: "",
+        img: "img/marina-bay-sands.jpg",
+        price: "400",
+        deal_price: "380",
+        currency: "S$",
+        star_rating: "5"
+    },
+    {
+        name: "Singapore Marriott Hotel",
+        loc: "Orchard Rd",
+        dist: "",
+        img: "img/marriott.jpg",
+        price: "360",
+        deal_price: "360",
+        currency: "S$",
+        star_rating: "5"
+    },
+    {
+        name: "Raffles Hotel",
+        loc: "Beach Rd",
+        dist: "",
+        img: "img/raffles-hotel.jpg",
+        price: "290",
+        deal_price: "290",
+        currency: "S$",
+        star_rating: "4"
+    },
+    {
+        name: "Rendezvous Hotel",
+        loc: "Bras Basah Rd",
+        dist: "",
+        img: "img/rendezvous-hotel.jpg",
+        price: "350",
+        deal_price: "350",
+        currency: "S$",
+        star_rating: "4"
+    },
+    {
+        name: "Trader's Hotel",
+        loc: "Cuscaden Rd",
+        dist: "",
+        img: "img/traders-hotel.jpg",
+        price: "220",
+        deal_price: "200",
+        currency: "S$",
+        star_rating: "3"
+    }
+];
