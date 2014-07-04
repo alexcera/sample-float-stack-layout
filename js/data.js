339f6ed7dcd1dc5042a340bb807d47db14540ef2
Alex Cera <alexcera007@gmail.com>
2014-07-04 13:08:07 +0800

    remove some unused css and js file. add some comments on the js. finalize the mechanism of constructing domFragments using a simple template

--- js/data.js
+++ js/data.js
@@ -1,119 +1,102 @@
 var DATA = [
     {
-        name: "Bay Hotel",
+        hotelName: "Bay Hotel",
         loc: "Telok Blangah Rd",
         dist: 10,
         img: "img/bay-hotel.jpg",
         price: 150,
-        deal_price: 150,
+        dealPrice: 150,
         currency: "$",
-        star_rating: 1
+        starRating: 1
     },
     {
-        name: "Crowne Plaza Hotel",
+        hotelName: "Crowne Plaza Hotel",
         loc: "Changi Airport",
         dist: 20,
         img: "img/crown-plaza.jpg",
         price: 275,
-        deal_price: 270,
+        dealPrice: 270,
         currency: "S$",
-        star_rating: 2
+        starRating: 2
     },
     {
-        name: "Dorsett Regency",
+        hotelName: "Dorsett Regency",
         loc: "New Bridge Rd",
         dist: 3,
         img: "img/dorset-regency.jpg",
         price: 260,
-        deal_price: 260,
+        dealPrice: 260,
         currency: "S$",
-        star_rating: 4
+        starRating: 4
     },
     {
-        name: "The Fullerton Hotel Singapore",
+        hotelName: "The Fullerton Hotel Singapore",
         loc: "Fullerton Square",
         dist: 7,
         img: "img/fullerton.jpg",
         price: 300,
-        deal_price: 300,
+        dealPrice: 300,
         currency: "S$",
-        star_rating: 3
+        starRating: 3
     },
     {
-        name: "Link Hotel",
+        hotelName: "Link Hotel",
         loc: "Tiong Bahru Rd",
         dist: 13,
         img: "img/link-hotel.jpg",
         price: 150,
-        deal_price: 150,
+        dealPrice: 150,
         currency: "S$",
-        star_rating: 1
+        starRating: 1
     },
     {
-        name: "Marina Bay Sands",
+        hotelName: "Marina Bay Sands",
         loc: "Bayfront Ave",
         dist: 11,
         img: "img/marina-bay-sands.jpg",
         price: 400,
-        deal_price: 380,
+        dealPrice: 380,
         currency: "S$",
-        star_rating: 5
+        starRating: 5
     },
     {
-        name: "Singapore Marriott Hotel",
+        hotelName: "Singapore Marriott Hotel",
         loc: "Orchard Rd",
         dist: 11,
         img: "img/marriott.jpg",
         price: 360,
-        deal_price: 360,
+        dealPrice: 360,
         currency: "S$",
-        star_rating: 5
+        starRating: 5
     },
     {
-        name: "Raffles Hotel",
+        hotelName: "Raffles Hotel",
         loc: "Beach Rd",
         dist: 8,
         img: "img/raffles-hotel.jpg",
         price: 290,
-        deal_price: 290,
+        dealPrice: 290,
         currency: "S$",
-        star_rating: 4
+        starRating: 4
     },
     {
-        name: "Rendezvous Hotel",
+        hotelName: "Rendezvous Hotel",
         loc: "Bras Basah Rd",
         dist: 9,
         img: "img/rendezvous-hotel.jpg",
         price: 350,
-        deal_price: 350,
+        dealPrice: 350,
         currency: "S$",
-        star_rating: 3
+        starRating: 3
     },
     {
-        name: "Trader's Hotel",
+        hotelName: "Trader's Hotel",
         loc: "Cuscaden Rd",
         dist: 12,
         img: "img/traders-hotel.jpg",
         price: 220,
-        deal_price: 200,
+        dealPrice: 200,
         currency: "S$",
-        star_rating: 3
+        starRating: 3
     }
 ];
-
-
-DATA.sortAscByProp = function(prop) {
-    var temp = this.slice(0);
-    temp.sort(function(a, b) {
-        return a[prop] - b[prop];
-    });
-    return temp;
-};
-
-DATA.sortDescByProp = function(prop) {
-    var temp = this.slice(0);
-    temp.sort(function(a, b) {
-        return b[prop] - a[prop];
-    });
-    return temp;
-}
--- js/index.js
+++ js/index.js
@@ -1,115 +1,212 @@
-window.onload = function() {
-    var select = document.getElementById("location"),
-        container = document.querySelector(".result-container");
+/**
+ * README:
+ *
+ * Regarding the design:
+ * I opted to use twitter-bootstrap for I honestly admit that my design skills are not excellent enough to come-up
+ * with a design given the time constraint.
+ *
+ * Regarding the js:
+ * This is a simple javascript implementation according to the problem presented.
+ * I resort on not using jQuery because reliance on such libraries may speed-up development process
+ * handle cross-browser bugs, but on the expense of not learning javascript on it's deepest sense. :)
+ * Anyway, if development speed and cross-browser compatibility is the main goal, resorting to such libraries will be 
+ * beneficial. As for this coding exam, I want to demonstrate my javascript ability. :)
+ *
+ * There are two approaches that comes to my mind as I am writing this implementation.
+ * The first is:
+ * 1.) of course through DOM-manipulation/building;
+ * 2.) second, is templating.
+ *
+ * There are many templating libraries out there which makes this task easy but for the sake of demo,
+ * I implemented a simple templating mechanism.
+ *
+ * I hardcoded the DATA in the form of object array, (data.js) for the sake of demo, 
+ * but in a real-world scenario, this DATA can be fetched via db calls. 
+ * 
+ * Anyway, coming from a java background, this is a cool task! :)
+ *
+ */
 
-    dataRenderer.showData(DATA, select.options[select.selectedIndex].value, container);
 
-    addEvent(select, "change", function() {
-        dataRenderer.showData(DATA, select.options[select.selectedIndex].value, container);
-    });
-};
-
-
-function addEvent(el, event_name, fn) {
+/**
+ * a cross-browser prop-detection for attaching events
+ * 
+ * @param {[type]}   el
+ * @param {[type]}   eventName
+ * @param {Function} fn
+ */
+function addEvent(el, eventName, fn) {
     //IE
     if (el.attachEvent)
-        el.attachEvent("on" + event_name, function() {
+        el.attachEvent("on" + eventName, function() {
             fn.call(el);
         });
     else if (el.addEventListener)
-        el.addEventListener(event_name, fn, false);
+        el.addEventListener(eventName, fn, false);
 }
 
+/**
+ * simple implementation of setting text,
+ * with cross-browser prop-detection in mind.
+ * 
+ * @param {[type]} el
+ * @param {[type]} text
+ */
+function setText(el, text) {
+    while (el.firstChild !== null)
+        el.removeChild(el.firstChild);
+    el.appendChild(document.createTextNode(text));
+}
+
+/**
+ * added a util function which replaces the
+ * first up to the nth match of the string.
+ *
+ * @param  {[type]} regex
+ * @param  {[type]} replacement
+ * @param  {[type]} nthOccurence
+ * @return {[type]}
+ */
+String.prototype.replaceByOccurence = function(regex, replacement, nthOccurence) {
+    if (nthOccurence > 0)
+        return this.replace(regex, replacement)
+            .replaceByOccurence(regex, replacement, --nthOccurence);
+    return this;
+};
+
 var dataRenderer = (function(w) {
+
+    var results, temp;
 
     function constructDOMFragment() {
 
         var fragment = document.createDocumentFragment();
-        for (var i = 0; i < results.length; i++)(function(r) {
 
-            var div_container = document.createElement("div");
-            div_container.className = "pull-left col-xs-4";
+        for (var i = 0; i < results.length; i++) {
 
-            var div_thumbnail = document.createElement("div");
-            div_thumbnail.className = "thumbnail";
+            var r = results[i],
+                containerNode = createStyledElement("div", "pull-left col-xs-4");
 
-            div_container.appendChild(div_thumbnail);
+            //FIRST APPROACH: templating
+            containerNode.innerHTML = temp.replace("#TITLE#", r.hotelName)
+                .replace("#IMGURL#", r.img)
+                .replace("#ALT#", r.hotelName)
+                .replaceByOccurence(/glyphicon-star-empty/, "glyphicon-star", r.starRating)
+                .replace(/#CURRENCY#/g, r.currency)
+                .replace("#PRICE#", r.price)
+                .replace("#DEALPRICE#", r.dealPrice);
 
-            var div_caption_top = document.createElement("div");
-            div_caption_top.className = "caption";
+            //SECOND APPROACH: DOM-building using fragments
+            /*thumbnailNode = createStyledElement("div", "thumbnail"),
+                topCaptionNode = createStyledElement("div", "caption"),
+                h5 = createStyledElement("div", ""),
+                imgNode = createStyledElement("img", "img-responsive"),
+                bottomCaptionNode = createStyledElement("div", "caption"),
+                dealPriceNode = createStyledElement("h3", "pull-right");
 
-            var h5 = document.createElement("h5");
-            h5["innerHTML" || "textContent"] = r.name;
-            div_caption_top.appendChild(h5);
-            div_thumbnail.appendChild(div_caption_top);
+            setText(h5, r.hotelName);
+            topCaptionNode.appendChild(h5);
+            thumbnailNode.appendChild(topCaptionNode);
+            imgNode.src = r.img;
+            imgNode.alt = r.hotelName;
+            thumbnailNode.appendChild(imgNode);
 
-            var img = document.createElement("img");
-            img.className = "img-responsive";
-            img.src = r.img;
-            img.alt = r.name;
-
-            div_thumbnail.appendChild(img);
-
-            var div_caption_bottom = document.createElement("div");
-            div_caption_bottom.className = "caption";
-
-            var starFragment = document.createDocumentFragment();
-            var stars = [];
-            for (var i = 5; i >= 1; i--) {
-                var star = document.createElement("span");
-                star.className = 5 - r.star_rating >= i ? "glyphicon glyphicon-star-empty" : "glyphicon glyphicon-star";
-                starFragment.appendChild(star);
-            };
-            div_caption_bottom.appendChild(starFragment);
-
-            var h3_deal_price = document.createElement("h3");
-            h3_deal_price.className = "pull-right";
-            if (r.price !== undefined && r.price && r.price != r.deal_price) {
-                var price = document.createElement("small");
+            if (r.price && r.price !== r.dealPrice) {
+                var priceNode = document.createElement("small");
                 var strikethrough = document.createElement("s");
-                strikethrough["innerHTML" || "textContent"] = r.currency + " " + r.price;
-                price.appendChild(strikethrough);
-                h3_deal_price.appendChild(price);
+                setText(strikethrough, r.currency + " " + r.price);
+                priceNode.appendChild(strikethrough);
+                dealPriceNode.appendChild(priceNode);
             }
-            h3_deal_price.appendChild(document.createTextNode("  " + r.currency + " " + r.price));
 
-            div_caption_bottom.appendChild(h3_deal_price);
+            bottomCaptionNode.appendChild(buildStarFragment(document.createDocumentFragment(), r.starRating));
+            dealPriceNode.appendChild(document.createTextNode("  " + r.currency + " " + r.dealPrice));
+            bottomCaptionNode.appendChild(dealPriceNode);
+            bottomCaptionNode.appendChild(createStyledElement("div", "clearfix"));
+            thumbnailNode.appendChild(bottomCaptionNode);
+            containerNode.appendChild(thumbnailNode);*/
 
-            var clearfix = document.createElement("div");
-            clearfix.className = "clearfix";
-            div_caption_bottom.appendChild(clearfix);
 
-            div_thumbnail.appendChild(div_caption_bottom);
-            fragment.appendChild(div_container);
+            fragment.appendChild(containerNode);
+        }
 
-        })(results[i]);
-
-        var clearfix = document.createElement("div");
-        clearfix.className = "clearfix";
-        fragment.appendChild(clearfix);
-
+        fragment.appendChild(createStyledElement("div", "clearfix"));
         return fragment;
     };
 
-    function showData(d, filter, container) {
+    function buildStarFragment(frag, rating) {
+        for (var i = 5; i >= 1; i--) {
+            frag.appendChild(createStyledElement("span", (5 - rating >= i) ? "glyphicon glyphicon-star-empty" : "glyphicon glyphicon-star"));
+        };
+        return frag;
+    };
+
+    /**
+     * a util function which creates element using the given
+     *  tag and className
+     * @param  {[type]} tag - example: "div"
+     * @param  {[type]} className
+     * @return {[type]} returns the HTMLElement
+     */
+    function createStyledElement(tag, className) {
+        var el = document.createElement(tag);
+        if (className)
+            el.className = className;
+        return el;
+    };
+
+    function clearContainer(c) {
+        while (c.hasChildNodes())
+            c.removeChild(c.firstChild);
+        return c;
+    };
+
+
+    /**
+     * Gets the data, applies some filter,
+     * construct the DOMFragment, clears the container
+     * and appends the DOMFragment to the container.
+     * 
+     * @param  {[type]} d
+     * @param  {[type]} filter
+     * @param  {[type]} container
+     * @param  {[type]} template
+     * @return {[type]}
+     */
+    function showData(d, filter, container, template) {
         results = d.slice(0);
+        temp = template;
+
         if (filter === "star_rating") {
             results.sort(function(a, b) {
-                return b.star_rating - a.star_rating;
+                return b.starRating - a.starRating;
             });
         } else if (filter === "dist") {
             results.sort(function(a, b) {
                 return a.dist - b.dist;
             });
         }
-        while (container.hasChildNodes()) {
-            container.removeChild(container.firstChild);
-        }
-        container.appendChild(constructDOMFragment());
+        clearContainer(container).appendChild(constructDOMFragment());
     };
 
+    /**
+     * Expose only this method which is the
+     * bread and butter of this function
+     */
     return {
         showData: showData
     };
 
 })(window);
+
+window.onload = function() {
+    var select = document.getElementById("location"),
+        container = document.querySelector(".result-container"),
+        template = document.querySelector(".template").innerHTML;
+
+    dataRenderer.showData(DATA, select.options[select.selectedIndex].value, container, template);
+
+    addEvent(select, "change", function() {
+        dataRenderer.showData(DATA, select.options[select.selectedIndex].value, container, template);
+    });
+};
